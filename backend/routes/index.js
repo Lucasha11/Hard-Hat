4f67d674251442ba701165335f210bb86526fb63
daniellencz <132099131+daniellencz@users.noreply.github.com>
2026-05-06 12:43:28 -0400

    Add stats and profile routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,16 @@
 import reviewRoutes from './review_routes.js';
 import authRoutes from './auth_routes.js';
 import searchRoutes from './search_routes.js';
+import statsRoutes from './stats_routes.js';
+import profileRoutes from './profile_routes.js';
 import devRoutes from './dev_routes.js'; // TODO: remove before production
 
 const constructorMethod = (app) => {
   app.use('/', devRoutes);
   app.use('/', authRoutes);
   app.use('/api', searchRoutes);
+  app.use('/', statsRoutes);
+  app.use('/', profileRoutes);
   app.use('/', reviewRoutes);
 
   app.use('*path', (req, res) => {
